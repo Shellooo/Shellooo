587e853d5b144128a3aaae86dbcb04bf8c01e564
Elizabeth Brown <ec.brown33@gmail.com>
2017-11-21 11:44:28 -0800

    got background animation working!

--- Shellooo/ViewController.swift
+++ Shellooo/ViewController.swift
@@ -10,16 +10,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var shellooBackground: UIImageView!
+    
+    var shellImages: [UIImage] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        shellImages = createImageArray(total: 295, imagePrefix: "shell")
+        animate(imageView: shellooBackground, images: shellImages)
+        
         // Do any additional setup after loading the view, typically from a nib.
+    
     }
 
+    func createImageArray(total: Int, imagePrefix: String) -> [UIImage] {
+    
+        var imageArray: [UIImage] = []
+        
+        for imageCount in 1..<total {
+            let imageName = "\(imagePrefix)-\(imageCount).jpg"
+            let image = UIImage(named: imageName)!
+       
+            imageArray.append(image)
+        }
+        return imageArray
+    }
+    func animate(imageView: UIImageView, images: [UIImage]) {
+        imageView.animationImages = images
+        imageView.animationDuration = 15
+        imageView.startAnimating()
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-
 }
+
 
